1c1e73f2e1a03ff3f875e3aa5fbb24ee40f185c4
GITHUB_revs314 <c28reva.kalbhor@afacademy.af.edu>
2026-03-02 13:09:07 -0700

    fixed code, trying again

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -40,22 +40,22 @@ architecture Behavioral of sevenseg_decoder is
 begin
     with i_Hex select
     o_seg <=
-        "0000001" when "0000",
-        "1001111" when "0001", -- when 1, light up b and c
-        "0010010" when "0010", -- when 2, light up all segments except for f and c
-        "0000110" when "0011", -- when 3, light up all segments except for f and e
-        "1001100" when "0100", -- when 4, light up all segments except for a and e and d
-        "0100100" when "0101", -- when 5, light up all segments except for b and e
-        "0100000" when "0110", -- when 6, light up all segments except for b
-        "0001111" when "0111", -- when 7, light up a, b, c
+        "1000000" when "0000",
+        "1111001" when "0001", -- when 1, light up b and c
+        "0100100" when "0010", -- when 2, light up all segments except for f and c
+        "0110000" when "0011", -- when 3, light up all segments except for f and e
+        "0011001" when "0100", -- when 4, light up all segments except for a and e and d
+        "0010010" when "0101", -- when 5, light up all segments except for b and e
+        "0000010" when "0110", -- when 6, light up all segments except for b
+        "1111000" when "0111", -- when 7, light up a, b, c
         "0000000" when "1000", -- when 8, light up all
-        "0000100" when "1001", -- when 9, light up all segments except for e and d
+        "0010000" when "1001", -- when 9, light up all segments except for e and d
         "0001000" when "1010", -- A
-        "1100000" when "1011", -- B 
-        "1110010" when "1100", -- C
-        "1000010" when "1101", -- D
-        "0110000" when "1110", -- E
-        "0111000" when "1111", -- F
+        "0000011" when "1011", -- B 
+        "0100111" when "1100", -- C
+        "0100001" when "1101", -- D
+        "0000110" when "1110", -- E
+        "0001110" when "1111", -- F
         "1111111" when others; -- all other illegal inputs -> blank display
-
+    
 end Behavioral;
