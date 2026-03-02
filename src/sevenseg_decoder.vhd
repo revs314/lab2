71415bdb19f8ca9b6a6b974db493288621510920
GITHUB_revs314 <c28reva.kalbhor@afacademy.af.edu>
2026-03-02 12:21:58 -0700

    wrote all code, cannot run bitstream

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -33,13 +33,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sevenseg_decoder is
     Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0); -- INPUT that is 4 bits HEX -> takes in values 0-F
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0)); -- OUTPUT that is 7 bits -> controls Sa - Sg
+           o_seg : out STD_LOGIC_VECTOR (6 downto 0)); -- OUTPUT that is 7 bits -> controls Sa - Sg
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
 begin
     with i_Hex select
-    o_seg_n <=
+    o_seg <=
         "0000001" when "0000",
         "1001111" when "0001", -- when 1, light up b and c
         "0010010" when "0010", -- when 2, light up all segments except for f and c
@@ -49,7 +49,7 @@ begin
         "0100000" when "0110", -- when 6, light up all segments except for b
         "0001111" when "0111", -- when 7, light up a, b, c
         "0000000" when "1000", -- when 8, light up all
-        "0001100" when "1001", -- when 9, light up all segments except for e and d
+        "0000100" when "1001", -- when 9, light up all segments except for e and d
         "0001000" when "1010", -- A
         "1100000" when "1011", -- B 
         "1110010" when "1100", -- C
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -79,23 +79,32 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
   -- declare the component of your top-level design
-
+    component sevenseg_decoder is
+        Port (
+            i_Hex : in std_logic_vector (3 downto 0);
+            o_seg: out std_logic_vector (6 downto 0)
+        );
+    end component sevenseg_decoder;
 
   -- create wire to connect button to 7SD enable (active-low)
-
+    signal w_7SD_EN_n : std_logic;
   
 begin
 	-- PORT MAPS ----------------------------------------
 
 	--	Port map: wire your component up to the switches and seven-segment display cathodes
 	-----------------------------------------------------	
-	-- 
+	decoder_uut : sevenseg_decoder port map (
+        i_Hex => sw,
+        o_seg => seg
+    );
 	
-	-- CONCURRENT STATEMENTS ----------------------------
-	
+	--CONCURRENT STATEMENTS
 	-- wire up active-low 7SD anode (active low) to button (active-high)
+	w_7SD_EN_n <= not btnC; -- maps the 4-bit input to the 7-bit output
 	-- display 7SD 0 only when button pushed
 	-- other 7SD are kept off
+	an  <= (0 => w_7SD_EN_n, others => '1');
 	-----------------------------------------------------
 	
 end top_basys3_arch;
